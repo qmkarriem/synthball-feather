e0f7c769d3bbac7f763e4de5a8a2bfe6bfa12bf0
qmkarriem <qmkarriem@gmail.com>
2019-05-28 16:26:47 -0400

    Update server-and-UDP.ino

--- server-and-UDP/server-and-UDP.ino
+++ server-and-UDP/server-and-UDP.ino
@@ -73,7 +73,7 @@ void setup() {
   delay(10000);
 
   // start the web server on port 80
-  server.begin();
+  //server.begin();
 
   // you're connected now, so print out the status
   printWiFiStatus();
@@ -124,7 +124,7 @@ void loop() {
     }
   }
   
-  WiFiClient client = server.available();   // listen for incoming clients
+  /*WiFiClient client = server.available();   // listen for incoming clients
 
   if (client) {                             // if you get a client,
     Serial.println("new client");           // print a message out the serial port
@@ -142,9 +142,9 @@ void loop() {
             // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
-            client.println();
+            client.println();*/
 
-            int packetSize = Udp.parsePacket();
+            /*int packetSize = Udp.parsePacket();
              if (packetSize)
   {
             client.print("Received packet of size ");
@@ -176,14 +176,14 @@ void loop() {
             client.println();
             // break out of the while loop:
             break;
-          }
+          }*/
           /*else {      // if you got a newline, then clear currentLine:
             currentLine = "";
           }
        
         else if (c != '\r') {    // if you got anything else but a carriage return character,
           currentLine += c;      // add it to the end of the currentLine
-        }*/
+        }
 
      
       }
@@ -191,7 +191,7 @@ void loop() {
     // close the connection:
     client.stop();
     Serial.println("client disconnected");
-  }
+  }*/
 }
 void printWiFiStatus() {
   // print the SSID of the network you're attached to:
