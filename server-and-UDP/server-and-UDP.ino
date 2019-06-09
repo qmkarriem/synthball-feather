ca354150da7424bc47dd765f6f469156b5fd58f6
qmkarriem <qmkarriem@gmail.com>
2019-06-09 11:08:16 -0400

    heart-rate detection + access point
    heart-rate detection & WiFi access point working on the same board, but not yet transmitting BPM over UDP

--- server-and-UDP/server-and-UDP.ino
+++ server-and-UDP/server-and-UDP.ino
@@ -20,6 +20,20 @@
 #include <WiFi101.h>
 #include <WiFiUdp.h>
 #include "arduino_secrets.h" 
+
+//from PulseSensor
+#define USE_ARDUINO_INTERRUPTS false
+#include <PulseSensorPlayground.h>
+//const int OUTPUT_TYPE = SERIAL_PLOTTER;
+const int PULSE_INPUT = A0;
+const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
+const int PULSE_FADE = 5;
+const int THRESHOLD = 550;   // Adjust this number to avoid noise when idle
+
+byte samplesUntilReport;
+const byte SAMPLES_PER_SERIAL_SAMPLE = 10;
+PulseSensorPlayground pulseSensor;
+
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
@@ -67,11 +81,58 @@ void setup() {
   // you're connected now, so print out the status
   printWiFiStatus();
   Udp.begin(localPort);
+
+  pulseSensor.analogInput(PULSE_INPUT);
+  pulseSensor.blinkOnPulse(PULSE_BLINK);
+  pulseSensor.fadeOnPulse(PULSE_FADE);
+
+  pulseSensor.setSerial(Serial);
+  //pulseSensor.setOutputType(OUTPUT_TYPE);
+  pulseSensor.setThreshold(THRESHOLD);
+
+  samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+  
+  // Now that everything is ready, start reading the PulseSensor signal.
+  if (!pulseSensor.begin()) {
+    /*
+       PulseSensor initialization failed,
+       likely because our Arduino platform interrupts
+       aren't supported yet.
+
+       If your Sketch hangs here, try changing USE_PS_INTERRUPT to false.
+    */
+    for(;;) {
+      // Flash the led to show things didn't work.
+      digitalWrite(PULSE_BLINK, LOW);
+      delay(50);
+      digitalWrite(PULSE_BLINK, HIGH);
+      delay(50);
+    }
+  }
 }
 
 
 void loop() {
   // if there's data available, read a packet
+  if (pulseSensor.sawNewSample()) {
+    /*
+       Every so often, send the latest Sample.
+       We don't print every sample, because our baud rate
+       won't support that much I/O.
+    */
+    if (--samplesUntilReport == (byte) 0) {
+      samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+
+      pulseSensor.outputSample();
+
+      /*
+         At about the beginning of every heartbeat,
+         report the heart rate and inter-beat-interval.
+      */
+      if (pulseSensor.sawStartOfBeat()) {
+        pulseSensor.outputBeat();
+      }
+    }
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
@@ -105,6 +166,7 @@ void loop() {
       //Serial.println("Device disconnected from AP");
     }
   }
+}
 }
 void printWiFiStatus() {
 
